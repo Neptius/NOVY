7249c997577ec34b0492139fc76c769b777d2ca1
Neptius <aymeric.legros@gmail.com>
2021-05-09 11:39:56 +0200

    up

--- apps/novy_site/lib/novy_site/live/home_live/index.ex
+++ apps/novy_site/lib/novy_site/live/home_live/index.ex
@@ -3,18 +3,9 @@ defmodule NovySite.HomeLive.Index do
 
   use NovySite, :live_view
 
-  alias NovyAdmin.Presence
-
-  @presence_guest "users:guest"
-
   @impl true
   def mount(_params, session, socket) do
-    if connected?(socket) do
-      {:ok, _} = Presence.track(self(), @presence_guest, UUID.uuid4(), %{})
-    end
-
     socket = assign_defaults(session, socket)
-
     {:ok, socket}
   end
 end
