d0a8fca1fa6910ce64a808f1f1de242075deb893
Neptius <aymeric.legros@gmail.com>
2021-05-09 20:13:37 +0200

    start

---
+++ apps/novy_admin/assets/esbuild.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const esbuild = require('esbuild')
+const fs = require('fs-extra');
+const autoprefixer = require("autoprefixer");
+const postCssPlugin = require("@deanc/esbuild-plugin-postcss");
+const tailwindcss = require("tailwindcss");
+
+const outdir = "./dist/"
+
+let startPlugin = {
+    name: 'example',
+    setup(build) {
+        build.onStart(async() => {
+            try {
+                console.log('build started')
+                await fs.rm(outdir, { recursive: true, force: true })
+                await fs.copy("./static/", outdir, { overwrite: true });
+            } catch (err) {
+                console.error(err)
+            }
+        })
+    },
+}
+
+esbuild.build({
+    entryPoints: ['ts/app.ts'],
+    platform: 'browser',
+    bundle: true,
+    metafile: true,
+    color: true,
+    format: 'iife',
+    outdir: 'dist/js/',
+    loader: {
+        ".ttf": "file"
+    },
+    external: ['*.ttf'],
+    plugins: [
+        startPlugin
+    ]
+}).catch((e) => console.error(e.message));
